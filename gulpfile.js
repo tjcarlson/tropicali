14d4ff9a86314b3fdc026587896e94e0f1866d75
Tim Carlson <writetimcarlson@gmail.com>
2019-02-19 21:17:58 -0800

    gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cleanCss = require("gulp-clean-css");
+sass.compiler = require("node-sass");
 
-gulp.task("default", function() {
-  // place code for your default task here
+gulp.task("sass", function() {
+  // we want to run sass css/app.scss app.css --watch
+  return gulp
+    .src("css/app.scss")
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("."));
 });
+
+gulp.task("watch", function() {
+  gulp.watch("css/app.scss", ["sass"]);
+});
+
+gulp.task("default", ["sass", "watch"]);
