47935bf80881fd1b5b43e0e90fcde4fe56d61868
Tim Carlson <writetimcarlson@gmail.com>
2019-02-20 19:00:09 -0800

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var cleanCss = require("gulp-clean-css");
+var cleanCSS = require("gulp-clean-css");
 sass.compiler = require("node-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var browserSync = require("browser-sync").create();
 
 gulp.task("sass", function() {
   // we want to run sass css/app.scss app.css --watch
   return gulp
     .src("css/app.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("."));
+    .pipe(
+      cleanCSS({
+        compatibility: "ie8"
+      })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("."))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("watch", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+
   gulp.watch("css/app.scss", ["sass"]);
 });
 
